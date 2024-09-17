2446d2ea87d2f1ddbf6764c166635f828a2db5ee
ThomasHni <thomas.hnizdo@gmail.com>
2024-09-17 20:52:36 +0200

    ajout pour github copilot

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -1,3 +1,14 @@
+-- ~/.config/nvim/lua/plugins.lua
 return {
+  -- Autres plugins...
 
+  {
+    "github/copilot.vim",
+    config = function()
+      -- Configuration spécifique au plugin si nécessaire
+      -- Par exemple, vous pouvez mettre :Copilot setup ici si nécessaire
+    end
+  },
+
+  -- Autres plugins...
 }
