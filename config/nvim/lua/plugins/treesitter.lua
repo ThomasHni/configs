6882ae47411cb6fa2d4801e0601ebf4bfea39ff2
ThomasHni <thomas.hnizdo@gmail.com>
2024-09-17 18:37:59 +0200

    Change le theme, ajoute plugin git et modif de la coloration de syntaxe

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,13 +1,13 @@
-return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-        local config = require("nvim-treesitter.configs")
-        config.setup({
-            ensure_installed = {"cpp", "c", "c_sharp", "cmake", "css", "doxygen", "html", "java", "javascript", "json", "lua", "markdown", "php", "python", "sql", "vim", "vimdoc", "xml", "yaml"},
-            highlight = { enable = true },
-            indent = { enable = true },
-        })
-    end
-}
-
+return 
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            local config = require("nvim-treesitter.configs")
+            config.setup({
+                auto_install = true,
+                highlight = { enable = true },
+                indent = { enable = true },
+            })
+        end
+    }
