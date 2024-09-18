bb2421a11394ace3caa1dd663e9f7596290d234e
ThomasHni <thomas.hnizdo@gmail.com>
2024-09-18 19:30:24 +0200

    Update neo-tree.lua

--- config/nvim/lua/plugins/neo-tree.lua
+++ config/nvim/lua/plugins/neo-tree.lua
@@ -1,4 +1,4 @@
-return{
+return {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
     dependencies = {
@@ -7,7 +7,11 @@ return{
         "MunifTanjim/nui.nvim",
     },
     config = function()
-        vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
+        -- Ouvrir ou fermer Neo-tree : <C-e>
+        vim.keymap.set('n', '<C-e>', ':Neotree toggle<CR>', { desc = "Toggle Neo-tree" })
+        -- Révéler le répertoire à gauche : <C-n>
+        vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', { desc = "Reveal Neo-tree" })
+        -- Remonter d'un niveau dans l'arborescence : <C-u>
+        vim.keymap.set('n', '<C-u>', ':Neotree reveal<CR>', { desc = "Go up in Neo-tree" })
     end
 }
-
