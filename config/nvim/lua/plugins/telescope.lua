52eac69d959bc924b0ce84647333dc8ab95e32cc
ThomasHni <thomas.hnizdo@gmail.com>
2024-09-18 19:30:51 +0200

    Update telescope.lua

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -1,8 +1,10 @@
-return{
+return {
     'nvim-telescope/telescope.nvim', tag = '0.1.5', dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
-    local builtin = require("telescope.builtin")
-    vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {}) -- Need ripgrep
-end
+        local builtin = require("telescope.builtin")
+        -- Chercher des fichiers : <C-f>
+        vim.keymap.set('n', '<C-f>', builtin.find_files, { desc = "Find Files" })
+        -- Recherche textuelle dans les fichiers : <C-g>
+        vim.keymap.set('n', '<C-g>', builtin.live_grep, { desc = "Live Grep" })
+    end
 }
